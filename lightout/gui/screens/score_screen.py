efc92eec2deeb753d56b56d91cdfa5e296115550
Victor Moura <victor_cmoura@hotmail.com>
2019-08-30 11:49:10 -0300

    Fixes bug with sorting gui

--- lightout/gui/screens/score_screen.py
+++ lightout/gui/screens/score_screen.py
@@ -3,6 +3,7 @@ import os
 import requests
 from firebase import Firebase
 import threading
+import time
 
 
 from kivy.uix.button import Button
@@ -10,6 +11,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import Screen
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.scrollview import ScrollView
+from kivy.uix.progressbar import ProgressBar
 
 from gui.models.table_entry import TableEntry
 
@@ -22,9 +24,15 @@ class ScoreScreen(Screen):
         self.pressed = None
         self.entries = []
         self.scores = []
-    
-        font = os.path.join('static', 'FreeSans.ttf')
 
+        self.build_screen()
+
+    def clear_widgets(self):
+        self.remove_widget(self.layout)
+        self.layout = None
+
+    def build_screen(self):
+        font = os.path.join('static', 'FreeSans.ttf')
         self.names = Button(
             text= u'Names',
             font_name = font,
@@ -58,36 +66,52 @@ class ScoreScreen(Screen):
         self.table = GridLayout(cols=3, size_hint_max_y=None)
         scroll.add_widget(self.table)
 
-        layout = BoxLayout(orientation='vertical')
-        layout.add_widget(header)
-        layout.add_widget(scroll)
+        self.layout = BoxLayout(orientation='vertical')
+        self.layout.add_widget(header)
+        self.layout.add_widget(scroll)
         
-        layout.add_widget(
+        self.layout.add_widget(
             Button(text='Back', size_hint_max_y=30, on_press=self.back)
         )
 
-        self.add_widget(layout)    
+        self.add_widget(self.layout)
+
+        self.pb = ProgressBar(value=0, max=100)
+        self.layout.add_widget(self.pb)
+
+
+    def simulate_loading(self, final_value):
+        while self.pb.value != final_value:
+            self.pb.value+=1
+            time.sleep(0.01)
 
     def update_scores_with_firebase(self):
+        self.pb.value = 0
         config = {
             "apiKey": "AIzaSyAdo9hLvVbA-KI7kwFlx2vOILV_o5K-dBE",
             "authDomain": "pa-project-1-graphs.firebaseapp.com",
             "databaseURL": "https://pa-project-1-graphs.firebaseio.com",
             "storageBucket": ""
         }
+        self.simulate_loading(10)
         firebase = Firebase(config)
+        self.simulate_loading(20)
         db = firebase.database()
+        self.simulate_loading(70)
         player_list = db.child("players").get()
         player_info_list = [player_list.val()[player_key] for player_key in player_list.val()]
+        self.simulate_loading(100)
         self.update(player_info_list)
+        self.scores = player_info_list
 
     def on_pre_enter(self):
+        if self.layout == None:
+            self.build_screen()
         fb_thread = threading.Thread(target=self.update_scores_with_firebase)
         fb_thread.start()
 
     def on_leave(self):
-        self.remove_widget(self.table)
-        self.table = GridLayout(cols=3, size_hint_max_y=None)
+        self.clear_widgets()
     
     def back(self, btn):
         self.parent.transition.direction = 'left'
@@ -111,12 +135,17 @@ class ScoreScreen(Screen):
             entry.time.text = score[2]
     
     def sort(self, btn):
-        if btn.text[-1] == u'▴':
-            btn.text = btn.text[:-1] + u'▾'
-            self.sort_descending(int(btn.id))
-        
+
+        if btn.text[-1] == u'▴' or btn.text[-1] == u'▾':
+            if btn.text[-1] == u'▴':
+                btn.text = btn.text[:-1] + u'▾'
+                self.sort_descending(int(btn.id))
+            
+            else:
+                btn.text = btn.text[:-1] + u'▴'
+                self.sort_ascending(int(btn.id))
         else:
-            btn.text = btn.text[:-1] + u'▴'
+            btn.text = btn.text + u'▾'
             self.sort_ascending(int(btn.id))
         
         self.rearrange()
